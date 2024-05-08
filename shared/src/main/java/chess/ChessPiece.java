7337e347c8bddbde3e3e4056b03c719fefee7509
gageboman23 <157763581+gageboman23@users.noreply.github.com>
2024-05-07 18:10:19 -0600

    All functions complete prior to implementing pieceMoves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -9,8 +9,12 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private ChessGame.TeamColor pieceColor;
+    private PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -28,15 +32,16 @@ public class ChessPiece {
     /**
      * @return Which team this chess piece belongs to
      */
+
     public ChessGame.TeamColor getTeamColor() {
-        return ChessGame.TeamColor();
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
