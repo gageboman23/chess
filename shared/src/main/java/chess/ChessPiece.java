5d754c063af2d3cab5f3c248d3da43b5fda0407e
gageboman23 <157763581+gageboman23@users.noreply.github.com>
2024-05-07 20:34:49 -0600

    Passed Bishop capture test!

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -19,6 +19,11 @@ class BishopMoveCalc implements PieceMoveCalc {
         int startRow = position.getRow();
         int startCol = position.getColumn();
 
+        //bishops color
+        ChessPiece MyPiece = board.getPiece(new ChessPosition(startRow, startCol));
+        ChessGame.TeamColor MyPieceColor = MyPiece.getTeamColor();
+
+
         for (int dRow : directions) {
             for (int dCol : directions) {
                 int row = startRow;
@@ -30,6 +35,17 @@ class BishopMoveCalc implements PieceMoveCalc {
 
                     // Check if the new position is within the bounds of the board
                     if (row < 1 || row > 8 || col < 1 || col > 8) {
+                        break;
+                    }
+
+                    ChessPiece pieceAtPosition = board.getPiece(new ChessPosition(row, col));
+
+                    if (pieceAtPosition != null) {
+                        // If it's an enemy piece, add the move (capture)
+                        if (pieceAtPosition.getTeamColor() != MyPieceColor) {
+                            moves.add(new ChessMove(position, new ChessPosition(row, col), null));
+                        }
+                        // Stop adding moves in this direction
                         break;
                     }
                     moves.add(new ChessMove(position, new ChessPosition(row, col), null));
@@ -61,11 +77,16 @@ public class ChessPiece {
         switch (type) {
             case BISHOP:
                 return new BishopMoveCalc();
+            case KING:
+                break;
             case QUEEN:
-                throw new RuntimeException("Not implemented");
+                break;
+            case KNIGHT:
+                break;
             case ROOK:
-                throw new RuntimeException("Not implemented");
-
+                break;
+            case PAWN:
+                break;
         }
         return null;
     }
