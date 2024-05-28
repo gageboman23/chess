79790b6b445a64f2f48889deaf5cafc03ce09bdd
gageboman23 <157763581+gageboman23@users.noreply.github.com>
2024-05-28 11:28:13 -0600

    Models and DAOs implemented

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,10 @@
+package dataaccess;
+import model.AuthData;
+
+public interface AuthDAO {
+    AuthData createAuth(String username) throws DataAccessException;
+    AuthData getAuth(String token) throws DataAccessException;
+    void deleteAuth(String token) throws DataAccessException;
+    void clear();
+
+}
