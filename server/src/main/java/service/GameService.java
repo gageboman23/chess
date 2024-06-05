88db178f8d72204b94c55c17c81b24551cd715cd
gageboman23 <157763581+gageboman23@users.noreply.github.com>
2024-06-04 23:01:38 -0600

    Empty Services

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.GameData;
+
+import java.util.Collection;
+
+public class GameService {
+    public Collection<GameData> listGames{}
+    public GameData createGame(String gameName) {}
+    public GameData joinGame(int gameID, String playerColor, String username){}
+}
