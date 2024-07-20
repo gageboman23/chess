f137b75222b21cdabca77bd6f8b5fa38ce291dd2
gageboman23 <157763581+gageboman23@users.noreply.github.com>
2024-07-19 20:23:25 -0600

    Local Auth DAO built. Build User DAO next.

--- server/src/main/java/dataaccess/AuthDAOBase.java
+++ server/src/main/java/dataaccess/AuthDAOBase.java
@@ -1,4 +1,11 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAOBase {
+
+    AuthData createAuth(String username) throws DataAccessException;
+    AuthData getAuth(String token) throws DataAccessException;
+    void deleteAuth(String token) throws DataAccessException;
+    void clear();
 }
--- server/src/main/java/dataaccess/LocalAuthDAO.java
+++ server/src/main/java/dataaccess/LocalAuthDAO.java
@@ -1,6 +1,47 @@
 package dataaccess;
 
-public class LocalAuthDAO {
+import model.AuthData;
 
-    //the actual datatype & access methods
+import java.util.HashMap;
+import java.util.UUID;
+
+public class LocalAuthDAO implements AuthDAOBase{
+
+    static HashMap<String, AuthData> authMap = new HashMap<>();
+
+
+    @Override
+    public AuthData createAuth(String username) throws DataAccessException {
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, username);
+        authMap.put(username, authData); // better to pair it with username or auth token?
+        return authData;
+    }
+
+
+    @Override
+    public AuthData getAuth(String username) throws DataAccessException {
+        if(authMap.get(username) != null) {
+            return authMap.get(username);
+        }
+        else {
+            throw new DataAccessException("Error: unauthorized");
+        }
+    }
+
+    @Override
+    public void deleteAuth(String username) throws DataAccessException {
+        if(authMap.get(username) != null) {
+            authMap.remove(username);
+        }
+        else {
+            throw new DataAccessException("Error: unauthorized");
+        }
+    }
+
+
+    @Override
+    public void clear() {
+        authMap.clear();
+    }
 }
