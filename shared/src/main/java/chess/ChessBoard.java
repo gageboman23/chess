ba850faf1dcca211173f8c8efc22032537227c6b
gageboman23 <157763581+gageboman23@users.noreply.github.com>
2024-07-24 09:20:36 -0600

    wow theyre strict

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -64,9 +64,6 @@ public class ChessBoard {
         return squares[position.getRow() - 1][position.getColumn() - 1];
     }
 
-    //public void removePiece(ChessPosition position) {
-        //squares[position.getRow() - 1][position.getColumn() - 1] = null;
-    //}
 
     /**
      * Sets the board to the default starting board
